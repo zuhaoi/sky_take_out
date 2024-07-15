ff1ca55ef6475cd0b3bdb72aae267c98f2f447ac
joke <11599969+jokehouse@user.noreply.gitee.com>
2024-07-15 16:36:32 +0800

    完成新增员工功能

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -9,6 +10,7 @@ import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,13 +24,13 @@ import java.util.Map;
 /**
  * 员工管理
  */
-@RestController
-@RequestMapping("/admin/employee")
-@Slf4j
-@Api(tags = "员工相关接口")
+@RestController  // 类注解，设置当前控制器类为RESTful风格，等同于@Controller与@ResponseBody两个注解组合功能
+@RequestMapping("/admin/employee")  //任意HTTP 请求映射到控制器方法上,类、方法注解
+@Slf4j  //日志输出，下面的log.info就是记录到了日志文件里面的信息
+@Api(tags = "员工相关接口")   //用在类上，例如Controller，表示对类的说明,接口文档可读性强
 public class EmployeeController {
 
-    @Autowired
+    @Autowired  //在属性、Setter 和构造器上使用自动装配
     private EmployeeService employeeService;
     @Autowired
     private JwtProperties jwtProperties;
@@ -39,9 +41,10 @@ public class EmployeeController {
      * @param employeeLoginDTO
      * @return
      */
-    @PostMapping("/login")
+    @PostMapping("/login")  //@RequestMapping 和 @ResponseBody的组合，post请求,将请求数据转换为Java对象，返回值为json或xml给客户端
+    @ApiOperation(value = "员工登录")   //用在方法上，说明方法的用途、作用
     public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
-        log.info("员工登录：{}", employeeLoginDTO);
+        log.info("员工登录：{}", employeeLoginDTO); //log日志对象实例，info日志级别
 
         Employee employee = employeeService.login(employeeLoginDTO);
 
@@ -70,6 +73,19 @@ public class EmployeeController {
      */
     @PostMapping("/logout")
     public Result<String> logout() {
+        return Result.success();
+    }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result save(@RequestBody EmployeeDTO employeeDTO){
+        log.info("新增员工：{}",employeeDTO);
+        employeeService.save(employeeDTO);
         return Result.success();
     }
 
